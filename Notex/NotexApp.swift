a357d416ef1718ab532745767dc0ca9acc6ba9d4
tanmayxx12 <troy12rollsza@gmail.com>
2025-02-25 16:38:55 +0530

    Initial Commit

---
+++ Notex/NotexApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotexApp.swift
+//  Notex
+//
+//  Created by Tanmay . on 25/02/25.
+//
+
+import SwiftUI
+
+@main
+struct NotexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
