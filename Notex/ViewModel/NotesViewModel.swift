140dc43f9f2b77ec7aebf13f0569d84d16beadfc
tanmayxx12 <troy12rollsza@gmail.com>
2025-02-28 16:47:45 +0530

    [Fix] Fixed the Navigated view for the HomeView so as to bifurcate the subcategory and category

--- Notex/ViewModel/NotesViewModel.swift
+++ Notex/ViewModel/NotesViewModel.swift
@@ -11,32 +11,102 @@ import Foundation
 
 @MainActor
 final class NotesViewModel: ObservableObject {
-    @Published var categories: [Category] = []
+    @Published var notes: [Note] = []
     private let context = CoreDataManager.shared.context
     
     init() {
         
     }
 
-    // Function to fetch categories:
-    func fetchCategories() {
-        let request = NSFetchRequest<Category>(entityName: "Category")
+    // MARK: - Function to Fetch Notes:
+    func fetchNotes(for subcategory: NoteSubCategory) -> [Note] {
+        let request = NSFetchRequest<Note>(entityName: "Note")
+        request.predicate = NSPredicate(format: "subcategory == %@", subcategory.rawValue)
+        request.sortDescriptors = [NSSortDescriptor(keyPath: \Note.timestamp, ascending: false)]
+        
         do {
-            categories = try context.fetch(request)
+            notes = try context.fetch(request)
+            return notes
         } catch {
-            print("There was an error fetching categories: \(error.localizedDescription)")
+            print("There was an error fetching notes: \(error.localizedDescription)")
+            return []
         }
-    }
-    
-    // Function to fetch subcategories for a given category:
-    func fetchSubcategory(for category: Category) -> [Subcategory] {
-        if let subcategory = category.toSubcategories?.allObjects as? [Subcategory] {
-            return subcategory.sorted{ $0.name! < $1.name! }
-        }
-        return []
+        
     }
     
     
+    // MARK: - Note Operations
+    func createNote(title: String, content: String, category: NoteCategory, subcategory: NoteSubCategory) {
+        let newNote = Note(context: context)
+        newNote.title = title
+        newNote.content = content
+        newNote.timestamp = Date()
+        newNote.category = category.rawValue
+        newNote.subcategory = subcategory.rawValue
+        
+        save()
+    }
+    
+    
+    
+    // Function to save:
+    func save() {
+        do {
+            try context.save()
+        } catch {
+            print("There was an error saving data: \(error.localizedDescription)")
+        }
+    }
+    
+}
 
+
+// MARK: - Enums for Category and Subcategory:
+enum NoteCategory: String {
+    case general = "General Categories"
+    case work = "Work And Productivity"
+    case personalAndLifeStyle = "Personal And Lifestyle"
+    case health = "Health And Fitness"
+    case finance = "Finance And Planning"
+    case shopping = "Shopping And Errands"
+    case social = "Social And Relationships"
+}
+
+enum NoteSubCategory: String {
+    // General:
+    case allNotes = "All Notes"
+    case uncategorized = "Uncategorised Notes"
+    
+    // work:
+    case work = "Work"
+    case meeting = "Meeting"
+    case tasks = "Tasks And To-Dos"
+    case projects = "Projects"
+    
+    // personal:
+    case personal = "Personal"
+    case dailyJournal = "Daily Journal"
+    case travel = "Travel"
+    case hobbies = "Hobbies"
+    case books = "Books To Read"
+    case movies = "Movies And TV Shows"
+    
+    // health:
+    case fitness = "Fitness"
+    case diet = "Diet And Meal Plans"
+    case mentalHealth = "Mental Health"
+    case medicalRecords = "Medical Records"
+    
+    // finance:
+    case budget = "Budget And Expenses"
+    case savings = "Savings Goals"
+    
+    // shopping:
+    case groceries = "Groceries"
+    case giftIdeas = "Gift Ideas"
+    
+    // social:
+    case friendsAndFamily = "Friends And Family"
+    case dateNightIdeas = "Date Night Ideas"
     
 }
--- Notex/Views/SubCategoryNotesView.swift
+++ Notex/Views/SubCategoryNotesView.swift
@@ -1,1 +1,112 @@
- 
+//
+//  SubCategoryNotesView.swift
+//  Notex
+//
+//  Created by Tanmay . on 28/02/25.
+//
+
+import SwiftUI
+
+struct SubCategoryNotesView: View {
+    @EnvironmentObject var viewModel: NotesViewModel
+    @State private var showingAddNoteSheet: Bool = false
+    
+    let category: NoteCategory
+    let subcategory: NoteSubCategory
+    
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                if viewModel.notes.isEmpty {
+                    VStack(spacing: 20) {
+                        Image(systemName: "note.text")
+                            .font(.system(size: 60))
+                            .foregroundStyle(.purple)
+                        Text("No Notes in \(subcategory.rawValue).")
+                            .font(.headline)
+                    }
+                } else {
+                    List {
+                        ForEach(viewModel.notes) { note in
+                            NavigationLink {
+                                Text("Note Detail View will come here")
+                            } label: {
+                                VStack {
+                                    Text("\(note.title ?? "")")
+                                        .font(.headline)
+                                    Text("\(note.timestamp ?? Date(), style: .date)")
+                                        .font(.caption)
+                                        .foregroundStyle(.gray)
+                                }
+                            }
+                            
+                            
+                        } // ForEach
+                    }
+                }
+            }
+            .navigationTitle(subcategory.rawValue)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Menu {
+                        Button("Edit") {}
+                        Menu {
+                            Button("Default (Date Edited)") {}
+                            Button("Date Edited") {}
+                            Button("Date Created") {}
+                            Button("Title") {}
+                            Divider()
+                            Button("Newest First") {}
+                            Button("Oldest First") {}
+                        } label: {
+                            HStack {
+                                Text("Sort By")
+                                Image(systemName: "arrow.up.arrow.down")
+                            }
+                        }
+                        
+                        Menu {
+                            Button("Default(On)") {}
+                            Button("On") {}
+                            Button("Off") {}
+                        } label: {
+                            HStack {
+                                Text("Group By Date")
+                                Image(systemName: "calendar")
+                            }
+
+                        }
+                        
+                    } label: {
+                        Image(systemName: "ellipsis.circle")
+                            .tint(.purple)
+                    }
+                    .disabled(viewModel.notes.isEmpty)
+                }
+                ToolbarItemGroup(placement: .bottomBar) {
+                    Spacer()
+                    
+                    NavigationLink {
+                        AddNoteView()
+                    } label: {
+                        Image(systemName: "square.and.pencil")
+                            .font(.title3)
+                            .tint(.purple)
+                    }
+
+                }
+                
+            }
+            
+        }
+        .onAppear {
+//            viewModel.fetchNotes(for: .uncategorized)
+        }
+    }
+}
+
+#Preview {
+    SubCategoryNotesView(category: .general, subcategory: .uncategorized)
+        .environmentObject(NotesViewModel())
+}
